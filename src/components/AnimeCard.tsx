be526400d4276b49e01d17e6bfa9d05fed856c25
Damian Cosmelli <damiancosmelli@gmail.com>
2025-03-08 09:33:06 +0000

    tooltip  "ver  video"

--- src/components/AnimeCard.tsx
+++ src/components/AnimeCard.tsx
@@ -12,14 +12,19 @@ interface CardProps {
         <div className="p-4">
             <h5 className="text-xl font-bold text-gray-900 dark:text-white truncate">{title}</h5>
         </div>
-        <a href={'https://www.youtube.com/watch?v=' + youtubeVideoId} 
+        <div className="relative group">
+            <a href={'https://www.youtube.com/watch?v=' + youtubeVideoId} 
                 target="_blank" rel="noopener noreferrer" className="hover:opacity-75">
-            <img className="w-full h-48 object-contain" src={imageUrl} alt={title} />
-        </a>
+                <img className="w-full h-48 object-contain" src={imageUrl} alt={title} />
+                <span className="absolute bottom-0 left-1/2 transform -translate-x-1/2 mb-2 bg-black text-white text-xs rounded px-2 py-1 opacity-0 group-hover:opacity-100 transition-opacity">
+                    Ver video
+                </span>
+            </a>
+        </div>
         <div className="p-2">
           <p className="text-gray-700 dark:text-gray-300">Episodios: {episodies} </p>
         </div>
-        </div>
+      </div>
     );
   }
 
