f30ca7fa90c4cb29f69e4310e9ee03b1cede4e21
Damian Cosmelli <damiancosmelli@gmail.com>
2025-03-09 09:14:13 +0000

    Add Service

---
+++ src/services/SeasonService.tsx
@@ -0,0 +1,14 @@
+export const animeSeason = async (season: string, seasonYear: string = new Date().getFullYear().toString()) => {
+
+    const ENDPOINT_SEASON_ANIME = new URL('https://kitsu.io/api/edge/anime');
+        ENDPOINT_SEASON_ANIME.searchParams.append("filter[season]",season);
+        ENDPOINT_SEASON_ANIME.searchParams.append("filter[seasonYear]",seasonYear);
+    
+    const response = await fetch(ENDPOINT_SEASON_ANIME.toString());
+    
+    if (!response.ok) {
+        throw new Error('Error fetching anime data');
+    }
+    const data = await response.json();
+    return data.data;
+};
