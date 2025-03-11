1d4e4f5ac362720785e5ebc3c48c26413fcc010b
Damian Cosmelli <damiancosmelli@gmail.com>
2025-03-11 06:18:09 +0000

    Fix error message

--- src/components/common/ErrorMessage.tsx
+++ src/components/common/ErrorMessage.tsx
@@ -4,7 +4,7 @@ interface ErrorMessageProps {
 
 export function ErrorMessage({ message }: ErrorMessageProps) {
   return (
-    <div className="bg-red-500 text-white p-4 rounded-lg shadow-md max-w-sm mx-auto">
+    <div className="bg-red-500 text-white p-4 rounded-lg shadow-md max-w-sm mx-auto mt-20">
       <p className="font-bold">Error:</p>
       <p>{message}</p>
     </div>
