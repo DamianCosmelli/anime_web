a549615b774e0fe59c4b4d3de13f2b78d45d262d
Damian Cosmelli <damiancosmelli@gmail.com>
2025-03-16 07:27:39 +0000

    se componiza el tutulo

--- src/components/Anime/AnimeInfo.tsx
+++ src/components/Anime/AnimeInfo.tsx
@@ -1,16 +1,16 @@
-import { CardProps } from './AnimeCard'
+import { CardProps } from './AnimeCard';
+import { AnimeTituloInfo } from './AnimeTituloInfo';
 
 export function AnimeInfo({ title, episodie, imageUrl, youtubeVideoId, synopsis }: CardProps) {
 
     return (
         <div className="container mx-auto p-4">
-            <h1 className="text-2xl font-bold">{title}</h1>
-            <p className="text-lg text-gray-700 gap-4">Episode: {episodie}</p>
+            <AnimeTituloInfo title={title} episodie={episodie} />
+                        
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
                 {/* Column 1: Anime Image */}
-                <div className="space-y-4">
-                    
-                    <img src={imageUrl} alt={title} className="w-full rounded-lg shadow-md" />
+                <div className="space-y-2">
+                    <img src={imageUrl} alt={title} className="w-9/10 rounded-lg shadow-md p-1" />
                 </div>
 
                 {/* Column 2: Synopsis */}
@@ -20,8 +20,9 @@ export function AnimeInfo({ title, episodie, imageUrl, youtubeVideoId, synopsis 
                 </div>
 
                 {/* Column 3: YouTube Video */}
-                <div>
+                <div className="space-y-2 flex flex-col">
                 <h2 className="text-xl font-bold">Trailer</h2>
+                {/** TODO: mnajar 1- Carga del video | 2- Poner imnagen si video no esta disponible */}
                     <iframe
                         width="100%"
                         height="315"
--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -11,7 +11,7 @@ export function Modal({ isOpen, onClose, children }: ModalProps) {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black/50 backdrop-blur-sm z-10">
-      <div className="bg-white p-6 rounded-lg shadow-lg relative m-2 w-3/4 h-9/10 overflow-auto">
+      <div className="bg-gray-500 p-6 rounded-lg shadow-lg relative m-2 w-3/4 h-9/10 overflow-auto">
         <button
           className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
           onClick={onClose}
