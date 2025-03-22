6f7d6a97a0a523db0dc73542cddb0a8b1ff9edf0
Damian Cosmelli <damiancosmelli@gmail.com>
2025-03-22 01:36:27 -0300

    Cambios de Layout

--- src/components/Layout/OldMainLayout.tsx
+++ src/components/Layout/OldMainLayout.tsx
@@ -15,20 +15,27 @@ interface LayoutProps {
 
 export function Layout({ children }: LayoutProps) {
   return (
-    <div className="flex min-h-screen flex-col  bg-gray-950">
-      <header className="flex items-center justify-between bg-amber-600">
+    <div className="flex min-h-screen flex-col  bg-gray-400">
+      <header className="bg-gray-900 p-3 w-full fixed z-10">
+        <div className="flex items-center justify-between">
+          <h1 className="text-white text-2xl font-bold font-serif">Anime-Web</h1>
+          <img src={logo} alt="Logo" className="w-10 h-10" />
+        </div>
+        <div className="flex items-center justify-between bg-amber-600">
+          <nav>
             <ul className="flex space-x-4">
-              <li><img src={logo} alt="Logo" className="w-10 h-10 ml-2" /></li>
               <li><MenuItem icon={iconTv} title="Anime en Tv" route="/" /></li>
               <li><MenuItem icon={iconTemporada} title='Anime Temporada' route='/temporada'/></li>
               <li><MenuItem icon={iconSearch} title='Buscar Anime' route='/search'/></li>
               <li><MenuItem icon={iconTop} title='Top Anime' route='/top'/></li>
             </ul>
+          </nav>
+        </div>
       </header>
       <div className="mt-1 flex-1 mb-12">
         <main className="p-4">{children}</main>
       </div>
-      <footer className="bg-gray-900 text-white p-1 text-center w-full">
+      <footer className="bg-gray-900 text-white p-1 text-center fixed bottom-0 w-full">
         <p>Anime-Web© 2025 by Bishops</p>
       </footer>
     </div>
