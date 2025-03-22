6d3770370845740cfefbfce67b0afeec8db7b2e1
Damian Cosmelli <damiancosmelli@gmail.com>
2025-03-22 02:18:49 -0300

    color y tamaño

--- src/components/common/LoadingSpinner.tsx
+++ src/components/common/LoadingSpinner.tsx
@@ -2,8 +2,10 @@ export function LoadingSpinner() {
   return (
     <div className="flex justify-center items-center min-h-screen">
       <div className="text-center" style={{ marginTop: '-80px' }}>
-        <p>Loading...</p>
-        <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin mt-2"></div>
+        <p className="text-white ">Loading ...</p>
+        <div className="w-24 h-24 border-4 border-blue-500 border-t-transparent rounded-full animate-spin mt-2">
+          
+        </div>
       </div>
     </div>
   );
