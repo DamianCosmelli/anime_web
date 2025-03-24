e22ab4ed5a0e9b15452cd4f623dff40f957955d8
Damian Cosmelli <damiancosmelli@gmail.com>
2025-03-24 02:50:02 -0300

    Refactor model Anime

--- src/components/Anime/AnimeGrid.tsx
+++ src/components/Anime/AnimeGrid.tsx
@@ -1,13 +1,10 @@
-import { Anime } from '../../hooks/useAimeTv';
-//import { AnimeCard } from './AnimeCard';
 import { TitlePage } from '../common/TitlePage';
 import { AnimeInfoModal } from './AnimeInfoModal';
-
+import { Anime } from '../../models/Anime'
 
 interface AnimeGridProps {
     animeList: Anime[];
     titlePage: string;
-    margin:boolean;
 }
 
 export function AnimeGrid({ animeList, titlePage}: AnimeGridProps) {
@@ -18,13 +15,7 @@ export function AnimeGrid({ animeList, titlePage}: AnimeGridProps) {
                 <ul className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
                    {animeList && animeList.map((anime: Anime) => (
                         <li key={anime.id} className="flex justify-center">
-                            <AnimeInfoModal
-                                title={anime.attributes.titles.en || anime.attributes.titles.ja_jp}
-                                episodie={anime.attributes.episodeCount || 'No Informado'}
-                                youtubeVideoId={anime.attributes.youtubeVideoId}
-                                imageUrl={anime.attributes.posterImage.small}
-                                synopsis={anime.attributes.synopsis}
-                            />
+                            <AnimeInfoModal anime={anime} />
                         </li>
                     ))}
                 </ul>
--- src/components/Anime/Titulos/AnimeTituloCard.tsx
+++ src/components/Anime/Titulos/AnimeTituloCard.tsx
@@ -1,21 +1,17 @@
 
-interface AnimeTituloCardProps {
-    title: string;
-    episodie: string; 
-    imageUrl: string;
-}
+import { Anime } from '../../../models/Anime'
 
-export function AnimeTituloCard({title, episodie, imageUrl}: AnimeTituloCardProps) {
+export function AnimeTituloCard({ anime }: { anime: Anime }) {
 
     return (
         <>     
-        <img className="w-full h-full object-cover" src={imageUrl} alt={title} />
+        <img className="w-full h-full object-cover" src={anime.attributes.posterImage.small} alt={anime.attributes.titles.en_jp || anime.attributes.titles.en} />
         <div className="absolute bottom-0 left-0 w-full bg-gray-800 opacity-70 text-white p-2 rounded-tr-lg">
             <h5 className="font-bold text-white truncate flex-grow text-xl sm:text-lg md:text-xl leading-tight">
-                {title}
+                {anime.attributes.titles.en_jp || anime.attributes.titles.en}
             </h5>
             <hr className="border-gray-300 dark:border-gray-700 my-2" />
-            <p className="text-white">Episodio: {episodie}</p>
+            <p className="text-white">Episodio:  {anime.attributes.episodeCount || 'No Informado'} </p>
         </div>
         </>
     );
