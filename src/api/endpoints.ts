18a91e373b03d36399e416577a3afe679384112e
Damian Cosmelli <damiancosmelli@gmail.com>
2025-04-20 16:12:42 -0300

    fix cantidad ainmes top

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -3,6 +3,6 @@ import { API_BASE_URL } from './config';
 export const ENDPOINTS = {
     ANIMES: `${API_BASE_URL}/anime`,
     SEASONS: `${API_BASE_URL}/seasons`,
-    TOP: `${API_BASE_URL}/top/anime?limit=25`,
+    TOP: `${API_BASE_URL}/top/anime?limit=20`,
  }
     // Agrega más endpoints según necesites 
