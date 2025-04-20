b1d4f3d24d35c78016cf473c711e6fc4aadff786
Damian Cosmelli <damiancosmelli@gmail.com>
2025-04-20 16:58:19 -0300

    fix titulo al cambiar temporadas

--- src/pages/AnimeSeason.tsx
+++ src/pages/AnimeSeason.tsx
@@ -1,53 +1,61 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 // Code to display the current anime on TV
 import { useAnimeSeason } from '../hooks/useAnimeSeason';
 import { ErrorMessage } from '../components/common/ErrorMessage';
 import { AnimeGrid } from '../components/Anime/AnimeGrid'
-import { Seasons } from '../models/Season'; 
+import { Seasons } from '../models/Season';
 import { AnimeSelectSeason } from "../components/Anime/Titulos/AnimeSelectSeason";
-import { useState } from 'react';
+import { useState , useEffect } from 'react';
 import { LoadingPuff } from '../components/common/LoadingPuff';
 import { getSeasonName } from '../utils/seasonConverts';
 
+
 function getSeasonYear() {
-    const date = new Date();
-    const year = date.getFullYear();
-    const seasonYears = [year, year - 1, year - 2, year - 3];
-    return seasonYears.reduce((acc, curr) => {
-        acc[curr.toString()] = curr.toString();
-        return acc;
-    }, {} as Record<string, string>);
-  }
+  const date = new Date();
+  const year = date.getFullYear();
+  const seasonYears = [year, year - 1, year - 2, year - 3];
+  return seasonYears.reduce((acc, curr) => {
+    acc[curr.toString()] = curr.toString();
+    return acc;
+  }, {} as Record<string, string>);
+}
 
-  interface dataSelect {
-    season:string,
-    seasonYear:string,
-  }
+interface dataSelect {
+  season: string,
+  seasonYear: string,
+}
 
-  function actualYear(): string {
-    const fechaActual = new Date();
-    return fechaActual.getFullYear().toString();
-  }
+function actualYear(): string {
+  const fechaActual = new Date();
+  return fechaActual.getFullYear().toString();
+}
 
 export function AnimeSeason() {
 
-   
-    const [seleccion , setSelection] = useState<dataSelect>({season:"winter" , seasonYear:actualYear()});
 
-    const handleFormSubmit = (data: dataSelect) => {
-        setSelection(data);
-    }
+  const [seleccion, setSelection] = useState<dataSelect>({ season: "winter", seasonYear: actualYear() });
+  const [titulo, setTitulo] = useState<string>("Anime de la Temporada " + getSeasonName(seleccion.season) + " del " + seleccion.seasonYear);
 
-    const { animeSeasonList, loading, error } = useAnimeSeason(seleccion.season, seleccion.seasonYear);
-       
-    if (loading) return <LoadingPuff />;
-    if (error) return <ErrorMessage message= {error}/>;
+  const handleFormSubmit = (data: dataSelect) => {
+    setSelection(data);
+    //setTitulo("Verificando...");
+  }
 
-    return (
-        <>
-          <div className='p-4'>
-             <AnimeSelectSeason  seasons={Seasons} years={getSeasonYear()} onSubmit={handleFormSubmit}/> 
-            <AnimeGrid animeList={animeSeasonList ?? []} titlePage={`Anime de la Temporada ${getSeasonName(seleccion.season)} del ${seleccion.seasonYear}`}/>
-          </div>
-        </>
-    );
+  const { animeSeasonList, loading, error } = useAnimeSeason(seleccion.season, seleccion.seasonYear);
+
+  useEffect(() => {    
+    setTitulo("Anime de la Temporada " + getSeasonName(seleccion.season) + " del " + seleccion.seasonYear);
+  }, [animeSeasonList]);
+
+  if (loading) return <LoadingPuff />;
+  if (error) return <ErrorMessage message={error} />;
+
+  return (
+    <>
+      <div className='p-4'>
+        <AnimeSelectSeason seasons={Seasons} years={getSeasonYear()} onSubmit={handleFormSubmit} />
+        <AnimeGrid animeList={animeSeasonList ?? []} titlePage={titulo} />
+      </div>
+    </>
+  );
 };
