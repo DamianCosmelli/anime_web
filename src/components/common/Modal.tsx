06f446030b2f091d421a222ac6ab67d88be7a49b
Damian Cosmelli <damiancosmelli@gmail.com>
2025-07-14 03:08:09 -0300

    se modifica modalInfo

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -11,13 +11,15 @@ export const Modal = ({ isOpen, onClose, children }: ModalProps) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black/50 backdrop-blur-sm z-10">
-      <div className="bg-gray-950 p-4 rounded-lg shadow-lg relative w-3/4 overflow-auto">
+    <div className="fixed inset-0 flex items-center justify-center backdrop-blur-sm z-10">
+      <div className="p-4 rounded-lg shadow-lg  overflow-auto">
         <button
-          className="absolute top-2 right-2 text-emerald-600 hover:text-amber-950"
+          className="absolute top-4 right-10 text-emerald-600 hover:text-emerald-300 items-center 
+          justify-center flex bg-white dark:bg-gray-950 rounded-full p-2 transition-colors duration-300  pl-4 pr-4"
           onClick={onClose}
         >
           <XMarkIcon className="w-6 h-6" />
+          Cerrar
         </button>
         {children}
       </div>
